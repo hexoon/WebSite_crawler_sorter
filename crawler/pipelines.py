def227067fa48a30d1fb125f5bb97cf4d8291e27
hexoon <nano.bit00@gmail.com>
2019-04-23 15:25:46 -0500

    Update pipelines.py

--- crawler/pipelines.py
+++ crawler/pipelines.py
@@ -13,6 +13,7 @@
 
 from pymongo import MongoClient
 from scrapy.conf import settings
+import bs4
 
 class MongoDBPipeline(object):
 
@@ -23,10 +24,35 @@ class MongoDBPipeline(object):
         db = connection[settings['MONGODB_DB']]
         self.collection = db[settings['MONGODB_COLLECTION']]
 
-    def clean_html(item):
+    def parse_body(self, divs):
+        body = ''
+        for div in divs: 
+            body = body + div.get_text()
+        body = body.split('\n')
+        body = [x.strip() for x in body if len(x.strip()) > 0]
+        body = ' *** '.join(body)
+        return body
 
+    def clean_html(self, obj):
+        clean_element = {}
+        item = bs4.BeautifulSoup(obj['html'])
+        try: item.find('nav').decompose()
+        except: print('<nav> item not found')
+        site_title = ''
+        try: site_title = item.title.get_text()
+        except: print('Title not found')
+        links = item.find_all('a')
+        site_urls = [ link['href'] for link in links if '#' not in link['href'] ]
+    
+        site_body = self.parse_body(item.find_all('div'))
+    
+        clean_element.update( {'url' : obj['url'],
+                               'site_name' : site_title,
+                               'site_urls' : site_urls,
+                               'site_body' : site_body} )
         return clean_element
 
     def process_item(self, item, spider):
-        self.collection.insert(dict(item))
+        clean_dict = self.clean_html(item)
+        self.collection.insert(dict(clean_dict))
         return item
